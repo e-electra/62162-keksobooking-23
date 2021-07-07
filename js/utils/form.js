d99c47d4732335477e313b0abf3d4d47b238d830
ErmolaevaEkaterina <ermolaevakatty@yandex.ru>
2021-07-07 21:37:15 +0300

    Правки по замечаниям)

--- js/utils/form.js
+++ js/utils/form.js
@@ -1,22 +1,22 @@
 const adForm = document.querySelector('.ad-form');
-const formElements = adForm.querySelector('fieldset');
+const formElements = adForm.querySelectorAll('fieldset');
 
 const mapFilters = document.querySelector('.map__filters');
 const mapFieldset = mapFilters.querySerlector('fieldset');
-const mapElements = mapFilters.querySerlector('select');
+const mapElements = mapFilters.querySerlectorAll('select');
 
 
 function deactivatePage() {
   adForm.classList.add('ad-form--disabled');
   mapFilters.classList.add('map__filters--disabled');
-  mapFieldset.setAttribute('disabled','disabled');
+  mapFieldset.setAttribute('disabled', true);
 
   formElements.forEach((element) => {
-    element.setAttribute('disabled','disabled');
+    element.setAttribute('disabled', true);
   });
 
   mapElements.forEach((element) => {
-    element.setAttribute('disabled','disabled');
+    element.setAttribute('disabled', true);
   });
 
 }
