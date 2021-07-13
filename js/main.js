d48e20b953b4af29086f3860686710083b29b903
ErmolaevaEkaterina <ermolaevakatty@yandex.ru>
2021-07-14 00:18:50 +0300

    Помощь друга

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
-import './advertisements.js';
-import './utils/form.js';
+import {deactivatePage} from './utils/form.js';
+import {resetMarker, map, mainPinMarker} from './map.js';
+
+const submitButton = document.querySelector('.ad-form__submit');
+const resetButton = document.querySelector('.ad-form__reset');
+
+document.addEventListener('DOMContentLoaded', () => {
+  deactivatePage();
+});
+
+submitButton.addEventListener('click', () => {
+  resetMarker(mainPinMarker, map);
+});
+
+resetButton.addEventListener('click', () => {
+  resetMarker(mainPinMarker, map);
+});
