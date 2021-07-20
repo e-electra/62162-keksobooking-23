39f3b53ceb9439e5b8df6aa2fde770b586460e76
ErmolaevaEkaterina <ermolaevakatty@yandex.ru>
2021-07-21 01:47:01 +0300

    Удаление неиспользуемых импортов

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
 import {adFormSubmit, adFormReset, deactivatePage} from './form.js';
-import {resetMarker, map, markerGroup, mainPinMarker, initAdvertsOnMap, updateFilteredMarkers} from './map.js';
+import {resetMarker, map, markerGroup, mainPinMarker, initAdvertsOnMap} from './map.js';
 import { getData } from './api.js';
 import {showSubmitSuccess, showSubmitError, showDataLoadError} from './notifications.js';
 import { ADVERTISEMENTS_STATE, SIMILAR_ADVERTISEMENT_COUNT } from './data.js';
-import { initFilters, filterAdverts, filtersForm, resetFilters } from './filters.js';
-
-const submitButton = document.querySelector('.ad-form__submit');
-const resetButton = document.querySelector('.ad-form__reset');
+import { initFilters, resetFilters } from './filters.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   deactivatePage();
